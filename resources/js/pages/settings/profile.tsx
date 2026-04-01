38b276442bfd663f6c003bd2601658788582e9e5
Ben-hur <mailem.meldavid@gmail.com>
2026-04-01 22:34:03 +0800

    feat: update hillbcs logo

--- resources/js/pages/settings/profile.tsx
+++ resources/js/pages/settings/profile.tsx
@@ -183,7 +183,7 @@ export default function Profile({
                     </CardContent>
                 </Card>
 
-                <DeleteUser />
+                {/* <DeleteUser /> */}
             </SettingsLayout>
         </AppLayout>
     );
